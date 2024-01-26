9a06511102b24291aab6acb33fcad03e618bc933
Michael DeFazio <michael.defazio@elastic.co>
2024-01-26 05:25:48 -0500

    Update indices table

--- src/components/TableIndices/TableIndices.jsx
+++ src/components/TableIndices/TableIndices.jsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/router';
 import {
   EuiBasicTable,
   EuiButton,
+  EuiBadge,
+  EuiHealth,
   EuiLink,
   EuiSearchBar,
   EuiSpacer,
@@ -16,37 +18,62 @@ const ITEMS = [
   {
     id: '01',
     name: 'Banana',
-    type: 'api-index'
+    type: 'api',
+    health: true,
+    connected: true,
+    docsCount: 280,
   },
   {
     id: '02',
     name: 'Apple',
-    type: 'api-index'
+    type: 'api',
+    health: true,
+    connected: true,
+    docsCount: 9438,
   },
   {
     id: '03',
     name: 'Mango',
-    type: 'crawlers'
+    type: 'crawler',
+    health: true,
+    connected: true,
+    docsCount: 2938,
   },
   {
     id: '04',
     name: 'Orange',
-    type: 'api-index'
+    type: 'api',
+    health: true,
+    connected: true,
+    docsCount: 84,
   },
   {
     id: '05',
     name: 'Papaya',
-    type: 'connectors'
+    type: 'connector',
+    health: false,
+    connected: true,
+    docsCount: 3020,
   },
   {
     id: '06',
     name: 'Pineapple',
-    type: 'crawlers'
+    type: 'crawler',
+    health: true,
+    connected: true,
+    docsCount: 1048,
   },
 ]
 
 const TableIndices = () => {
   const router = useRouter();
+
+  const viewIndex = (index) => {
+    console.log(index)
+  }
+  const deleteIndex = (index) => {
+    console.log(index)
+  }
 
   const columns = [
 
@@ -55,18 +82,78 @@ const TableIndices = () => {
       name: 'Name',
       'data-test-subj': 'nameCell',
       render: (name, entry) => {
+
+        let slug;
+
+        if (entry.type === "connector") {
+          slug = 'connectors';
+        } else if (entry.type === "crawler") {
+          slug = 'crawlers'
+        } else {
+          slug = 'api-index'
+        }
+
+
         return (
-          <EuiLink onClick={() => router.push(`/content/${entry.type}/overview?title=${name}`)}>
+          <EuiLink onClick={() => router.push(`/content/${slug}/overview?title=${name}`)}>
             {name}
           </EuiLink>
         )
       },
     },
     {
-      field: 'type',
-      name: 'Source type',
-      'data-test-subj': 'typeCell',
+      field: 'health',
+      name: 'Index health',
+      'data-test-subj': 'healthCell',
+      width: '150px',
+      render: (health) => {
+        return (
+          <EuiHealth textSize="s" color={health ? "success" : "danger"}>{health ? "Healthy" : "Errors"}</EuiHealth>
+        )
+      },
     },
+    {
+      field: 'docsCount',
+      name: 'Docs count',
+      'data-test-subj': 'docsCountCell',
+    },
+    {
+      field: 'type',
+      name: 'Ingestion method',
+      'data-test-subj': 'typeCell',
+      render: (type) =>
+        (<span style={type === 'api' ? { textTransform: 'uppercase' } : { textTransform: "capitalize" }}>{type}</span>)
+    },
+    {
+      field: 'connected',
+      name: 'Ingestion status',
+      'data-test-subj': 'ingestStatusCell',
+      render: (status) => {
+        return (
+          <EuiBadge color={status ? "success" : "danger"}>{status ? "Connected" : "Not connected"}</EuiBadge>
+        )
+      },
+    },
+    {
+      name: 'Actions',
+      actions: [
+        {
+          name: <span>View</span>,
+          description: 'View index',
+          icon: 'eye',
+          type: 'icon',
+          onClick: viewIndex,
+        },
+        {
+          name: <span>Delete</span>,
+          description: 'Delete index',
+          icon: 'trash',
+          color: 'danger',
+          type: 'icon',
+          onClick: deleteIndex,
+        },
+      ]
+    }
   ]
 
   return (
